0c93aaa9cf1ec7b8458e317e04cf916a5ac52c9f
Simon Nguyen <simon@simtechmedia.com>
2013-03-30 19:33:55 +1100

    finally got the regex to work

--- js/app.js
+++ js/app.js
@@ -7,15 +7,35 @@ var app = angular.module("angtwit", ['ui','ngSanitize'] , function($routeProvide
 app.directive("tweet", function(){
     return {
         restrict:"E",
-        template:"<div class=\"tweetprofilepic\">\n    <img src=\"{{tweet.user.profile_image_url}}\">\n</div>\n<div class=\"tweetdetails\">\n    <p>ScreenName : @{{tweet.user.screen_name}} </p>\n</div>\n\n<hr/>"
+        controller: function ($scope) {
+            this.sayHi = function() {
+                console.log("Hello");
+            }
+        },
+        template:"<div class=\"tweetprofilepic\">\n    <img ng-src=\"{{tweet.user.profile_image_url}}\">\n</div>\n<div class=\"tweetdetails\">\n    <p><a href=\"#\"><strong>{{tweet.user.name}}</strong><span class=\"screen_name\">@{{tweet.user.screen_name}}</span></a></p>\n    <tweetbody ng-bind-html=\"testFunc(tweet.text)\"></tweetbody>\n</div>\n<hr/>"
     }
 });
 
-function FriendTweetsCtrl($scope, $http)
+app.directive("tweetbody", function(){
+    return {
+        restrict:"E",
+        link: function( scope , element, attrs ) {
+            //console.log(element[0].innerHTML);
+        }
+    }
+});
+
+function FriendTweetsCtrl( $scope , $http )
 {
     $http.get('data/HomeTweets.json').success(function(data) {
         var results = data.query.results.statuses.status;
         $scope.tweets = results;
-        console.log(data);
     });
+
+    $scope.testFunc = function( st ){
+        var regEx = new RegExp("@([a-z0-9_]{1,20})","g");
+        var newString = st.replace(  regEx , "<a href='#'>@$1</a>" );
+        return newString;
+    }
 }
+
