3386fc099006f04f5fa179976eed27ffcf785253
Simon Nguyen <simon@simtechmedia.com>
2013-03-31 22:08:39 +1100

    finished up serach colluns

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 var app = angular.module("angtwit", ['ui','ngSanitize'] , function($routeProvider, $locationProvider) {
 
-
 });
 
-
-app.directive("tweet", function(){
+app.factory('ColumnData', function() {
     return {
-        restrict:"E",
-        controller: function ($scope) {
-            this.sayHi = function() {
-                console.log("Hello");
+        "columns" : [
+            {
+                "name" : "home",
+                "type" : "friendTweets"
+            },
+            {
+                "name" : "angularJS",
+                "type" : "search"
             }
-        },
-        template:"<div class=\"tweetprofilepic\">\n    <img ng-src=\"{{tweet.user.profile_image_url}}\">\n</div>\n<div class=\"tweetdetails\">\n    <p><a href=\"#\"><strong>{{tweet.user.name}}</strong><span class=\"screen_name\">@{{tweet.user.screen_name}}</span></a></p>\n    <tweetbody ng-bind-html=\"testFunc(tweet.text)\"></tweetbody>\n</div>\n<hr/>"
+        ]
     }
 });
 
@@ -25,16 +26,112 @@ app.directive("tweetbody", function(){
     }
 });
 
-function FriendTweetsCtrl( $scope , $http )
+
+app.directive("loadtweets",function(){
+    return {
+        restrict:"E",
+        template:"<p>Load More Tweets</p>",
+        link: function( scope , element, attrs ) {
+            element.addClass("loadmoretweets");
+            element.bind("click", function(){
+                scope.$apply("addMorePages()");
+            })
+        }
+    }
+});
+
+
+app.directive( "tweetcolumn" , function ($compile) {
+    var template_for = function(type) {
+        return type+"\\.html";
+    };
+    return {
+        restrict:"E",
+        transclude: true,
+        scope:true,
+        compile: function(element, attrs) {
+            return function(scope, element, attrs) {
+                // Waits for Attibutes to be ready
+                attrs.$observe('type', function(value) {
+                    // Switch for Column Twitter Type
+                    var tmpl;
+                    switch (value)
+                    {
+                        case "friendTweets":
+                            tmpl = template_for("friendtweets-column");
+                            break;
+                        case "search":
+                            tmpl = template_for("search-column");
+                            break;
+                    }
+                    // Switch for Column Type
+                    element.html($("#"+tmpl).html());//.show();
+                    $compile(element.contents())(scope);
+                });
+            };
+        }
+    }
+})
+
+function ColumnsCtrl ( $scope, ColumnData )
 {
+    $scope.ColumnData = ColumnData;
+    $scope.test = function(){
+        console.log("ohi");
+    }
+    //console.log(ColumnData.columns.length);
+}
+
+function SeatchTweetsCtrl ( $scope , $http , ColumnData )
+{
+    $scope.data = ColumnData;
+
+    // Limit for pagination
+    $scope.pageSize = 20;
+
+    $http.get('data/searchangularjs.json').success(function(data) {
+        var results = data.query.results.results;
+        $scope.tweets = results;
+    });
+
+
+    $scope.addMorePages = function() {
+        console.log("addingMorePages");
+        $scope.pageSize=$scope.pageSize+20
+    }
+
+    $scope.addHTML = function( st ){
+
+        // Add Ancor to HTTP
+        var regEx = /@([a-z0-9_]{1,20})/gi;
+        var newString = st.replace(  regEx , "<a href='#'>@$1</a>" );
+
+        // Adds Ancor to links
+        var htmlRegEx = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        newString = newString.replace(  htmlRegEx , "<a href='$1'>$1</a>" );
+
+        return newString;
+    }
+}
+
+function FriendTweetsCtrl( $scope , $http , ColumnData )
+{
+    $scope.data = ColumnData;
     $http.get('data/HomeTweets.json').success(function(data) {
         var results = data.query.results.statuses.status;
         $scope.tweets = results;
     });
 
-    $scope.testFunc = function( st ){
-        var regEx = new RegExp("@([a-z0-9_]{1,20})","g");
+    $scope.addHTML = function( st ){
+
+        // Add Ancor to HTTP
+        var regEx = /@([a-z0-9_]{1,20})/gi;
         var newString = st.replace(  regEx , "<a href='#'>@$1</a>" );
+
+        // Adds Ancor to links
+        var htmlRegEx = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        newString = newString.replace(  htmlRegEx , "<a href='$1'>$1</a>" );
+
         return newString;
     }
 }
