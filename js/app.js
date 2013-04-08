a0c31380e4025142447640884d86fc95b7e03dec
Simon Nguyen <simon@simtechmedia.com>
2013-04-08 19:04:24 +1000

    got the modal buttons up

--- js/app.js
+++ js/app.js
@@ -20,7 +20,13 @@ app.factory('ColumnData', function() {
                 "name" : "@simtechmedia",
                 "type" : "search" ,
                 "settings" : false
+            },
+            {
+                "name" : "simtechmedia",
+                "type" : "user" ,
+                "settings" : false
             }
+
         ]
     }
 });
@@ -125,6 +131,10 @@ app.directive( "tweetcolumn" , function ($compile) {
                         case "search":
                             tmpl = template_for("search-column");
                             break;
+                        case "user":
+                            tmpl = template_for("user-column");
+                            break;
+
                     }
                     // Switch for Column Type
                     element.html($("#"+tmpl).html());//.show();
@@ -164,13 +174,11 @@ app.directive('editcol', function() {
     return {
         restrict:"A",
         link : function(scope , element) {
-
+            var originalElement = element.children();
             var magicon = "<i class=\"icon-search icon-white heading-icon\" editcol></i>";
             var edit    = "<p class=\"heading-text\" editcol><a href=\"#\">Edit</a></p>";
             var close   = "<p class=\"heading-text\" editcol><a href=\"#\">Close</a></p>";
-
             if(scope.showSettings.show == true) openEditState();
-
             scope.getShowSettings = function () {
                 return { show : scope.showSettings.show }
             };
@@ -188,7 +196,8 @@ app.directive('editcol', function() {
             }
 
             function closeEditState(){
-                element.children().replaceWith(magicon);
+               //element.children().replaceWith(magicon);
+               element.children().replaceWith(originalElement);
             }
 
             /*
@@ -226,6 +235,23 @@ app.directive('searchsettings', function(){
         }
     }
 })
+
+app.directive('usersettings', function(){
+    return {
+        restrict:"E",
+        templateUrl:"partials/user-settings.html",
+
+        link: function(scope, element)
+        {
+            scope.getSettings = function (){
+                return {
+                    "settingsVal" : scope.showSettings
+                };
+            }
+        }
+    }
+})
+
 
 // Controller to share the column data to the repeater that creates the columns
 // think i can get away with incorporating this to something else eventually, feel unnessary
@@ -273,6 +299,42 @@ function SeatchTweetsCtrl ( $scope , $http , ColumnData )
     }
 }
 
+function UserTweetsCtrl ( $scope , $http , ColumnData )
+{
+    console.log("UserTweetsCtrl");
+
+    $scope.data             = ColumnData;
+    // Limit for pagination
+    $scope.pageSize         = 10;
+    $scope.searchString     = "";
+    var searchString        = encodeURI($scope.$parent.column.name);
+    var searchURL           = "http://search.twitter.com/search.json?q="+searchString+"&rpp=20&include_entities=true&result_type=mixed"+"?callback=JSON_CALLBACK";
+
+    // Debug stuff, getting rate limited
+    if(document.URL.substring(0,16) == "http://localhost")
+    {
+        $http.get('data/userTweets.json').success(function(data) {
+            $scope.processData(data);
+        });
+    } else {
+        $http.jsonp(searchURL).success(function(data)
+        {
+            $scope.processData(data);
+        })
+    }
+
+    $scope.processData = function(data)
+    {
+        var results = JSON.parse(data.query.results.result);
+        $scope.tweets = results;
+    }
+
+    $scope.addMorePages = function() {
+        console.log("addingMorePages");
+        $scope.pageSize=$scope.pageSize+10
+    }
+}
+
 // Controller for searchColumn
 function searchSettingsCtrl ( $scope , ColumnData)
 {
@@ -301,7 +363,7 @@ function searchSettingsCtrl ( $scope , ColumnData)
     // Toggle Settings Panel
     $scope.toggleSettingsPanel = function()
     {
-        console.log("toggleSettingsPanel");
+//        console.log("toggleSettingsPanel");
         ($scope.currentVO.settings == true ) ? $scope.closeSettingsPanel() : $scope.openSettingsPanel() ;
     }
 
@@ -327,7 +389,9 @@ function searchSettingsCtrl ( $scope , ColumnData)
     $scope.deleteColumn = function ()
     {
         // Underscore.js function
-        $scope.data.columns = _($scope.data.columns).reject(function(el) {  return el.name === $scope.currentVO.name ; });
+        $scope.data.columns = _($scope.data.columns).reject(function(el) {
+            return el.name === $scope.currentVO.name && el.type == $scope.currentVO.type ;
+        });
     }
 }
 
@@ -349,15 +413,11 @@ function FriendTweetsCtrl( $scope , $http , ColumnData )
         })
     }
 
+    // Process JSON data and push it to the view
     $scope.processData = function(data)
     {
         var results = data.query.results.statuses.status;
         $scope.tweets = results;
-    }
-
-    $scope.addHTML = function( st ){
-        //return addLinksToHtml(st);
-        return st;
     }
 }
 
@@ -373,7 +433,7 @@ function TopBarCtrl (  $scope , $http , ColumnData )
 }
 
 // Search More Tweets Controller
-function TopSearchBarCtrl ($scope, ColumnData,  $http)
+function TopSearchBarCtrl ($scope, ColumnData )
 {
     $scope.ColumnData       = ColumnData
     $scope.content;                         // Content
@@ -386,24 +446,25 @@ function TopSearchBarCtrl ($scope, ColumnData,  $http)
         ($scope.searchVars.searchString.length > 2 ) ?  $scope.searchDisabled = false :  $scope.searchDisabled = true;
     }
     //Search Tweets
-    $scope.searchTweets = function()
-    {
+    $scope.searchTweets = function()  {
         $scope.ColumnData.columns.push({
             "name" : $scope.searchVars.searchString ,
             "type" : "search",
             "settings" : false
         });
     }
+    /*
     // Popover Content
     // Commenting out , might use it later
-    /*
+
     $scope.popover = {
         "content": "<div ng-repeat=\"tweet in tweets\" class=\"tweetBox\">\n    <div class=\"tweetprofilepic\">\n        <img ng-src=\"{{tweet.profile_image_url}}\">\n    </div>\n    <div class=\"tweetdetails\">\n        <p><a href=\"#\"><strong>{{tweet.from_editcol_name}}</strong><span class=\"screen_name\">@{{tweet.from_user}}</span></a></p>\n        <tweetbody ng-bind-html=\"addHTML(tweet.text)\">{{tweet.text}}</tweetbody>\n    </div>\n    <hr/>\n</div>"
     }
     */
 }
 
-
+// Controller for all the hash tags, going to figure out a way
+// So one controller does the whole tweet rather than individual elements
 function HashTagSearchCtrl ($scope, ColumnData )
 {
     $scope.ColumnData       = ColumnData
@@ -411,9 +472,8 @@ function HashTagSearchCtrl ($scope, ColumnData )
     $scope.searchVars       = {
         searchString : ""
     }
-
     //Search Tweets
-    $scope.searchTweets = function(st)
+    $scope.searchTweets     = function(st)
     {
         $scope.ColumnData.columns.push({
             "name" : st ,
@@ -426,17 +486,20 @@ function HashTagSearchCtrl ($scope, ColumnData )
 
 // Controller for Profile Modal
 // Uses jsonp call to grab data from ypl
-function MyModalCtrl ( $scope, $http )
+function ProfileModalCtrl ( $scope, $http , ColumnData )
 {
    // var id = $scope.$parent.tweet.user.id;
-
-    $scope.sourceTweet = $scope.$parent.tweet ;
+    $scope.ColumnData       = ColumnData
+    $scope.sourceTweet      = $scope.$parent.tweet ;
 
     var id;
+    var screen_name;
     if($scope.$parent.tweet.from_user_id != undefined) {
-        id = $scope.$parent.tweet.from_user_id;
+        id =                $scope.$parent.tweet.from_user_id;
+        screen_name =       $scope.$parent.from_user
     } else {
         id = $scope.$parent.tweet.user.id;
+        screen_name =       $scope.$parent.tweet.user.screen_name;
     }
 
     $scope.urlRequest = "https://query.yahooapis.com/v1/public/yql?q=set%20oauth_token%3D'109847094-V2IhnURLzb4q9bpvbMAuvulrNywM4EvSvmjsILvV'%20on%20twitter%3B%0Aset%20oauth_token_secret%3D'5W73pwttktohZ55YOFC7Tjl9YQeelyQ6bfDrDDsZLc'%20on%20twitter%3B%0Aselect%20*%20from%20twitter.users%20where%20id%3D"+id+"%3B%20&format=json&env=store%3A%2F%2Fsimtechmedia.com%2Foauthdemo&callback=JSON_CALLBACK";
@@ -473,6 +536,24 @@ function MyModalCtrl ( $scope, $http )
                 $scope.getLocalProfile();
                 //$scope.user = results.user;
             }
+        });
+    }
+
+    $scope.userTweets = function()
+    {
+        $scope.ColumnData.columns.push({
+            "name" : screen_name ,
+            "type" : "user",
+            "settings" : false
+        });
+    }
+
+    $scope.userMentions = function()
+    {
+        $scope.ColumnData.columns.push({
+            "name" : "@"+screen_name ,
+            "type" : "search",
+            "settings" : false
         });
     }
 }
